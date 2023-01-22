82f1cc8e9ac67e6357fb306572a7fdb1fa127e86
boon4681 <46645814+boon4681@users.noreply.github.com>
2023-01-23 00:08:19 +0700

    burh

--- src/cardboard/tokenizer/basic.ts
+++
@@ -1,7 +0,0 @@
-import { Reader } from "../base/reader"
-import { Wrapper } from "../base/wrapper"
-
-export const header = new Wrapper('header')
-export const hidden = new Reader('hidden', /[\s\r\n]*/, { mode: 'normal', ignored: false })
-export const Hidden = new Reader('hidden', /[\s\r\n]+/, { mode: 'normal', ignored: false })
-export const identifier = new Reader('identifier', /[_\w][_\w\d]*/)
